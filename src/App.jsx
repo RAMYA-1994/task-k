e6356b1405265af7db3adcd39361cddaa58366eb
RAMYA-1994 <ramyarrr2016@gmail.com>
2025-02-14 14:16:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FlowBuilder from"./components/FlowBuilder"
+
+function App() {
+  return <>
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+
+      <FlowBuilder/>
+      <Sidebar/>
+
+      </div>
+      </>
+}
+
+export default App
